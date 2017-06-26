ba9141ddd157ee0be746e8bcd3382b27fa1f8fd5
vadimvshepelev <vadimvshepelev@gmail.com>
2017-06-27 00:49:49 +0300

    add 'research' template

--- dcmtsite/www/views.py
+++ dcmtsite/www/views.py
@@ -7,3 +7,6 @@ def index(request):
     people = Person.objects.all()
     # return render(request, 'index.html')
     return render(request, 'index.html', {'people': people})
+
+def research(request):
+    return render(request, 'research.html')
