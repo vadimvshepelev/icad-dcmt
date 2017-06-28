960499d5ec40a51272754db85a25fdaf3caa8154
vadimvshepelev <vadimvshepelev@gmail.com>
2017-06-28 14:37:08 +0300

    add paper type splitting on publications page

--- dcmtsite/www/views.py
+++ dcmtsite/www/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Person, Paper
+from datetime import date
 
 # Create your views here.
 
@@ -14,10 +15,22 @@ def research(request):
 	
 def publications(request):
     people = Person.objects.all()
-    publications = Paper.objects.all()
+    year_oldest = 2000
+    year_cur = date.today().year
+    years = range(year_oldest, year_cur)
     articles = Paper.objects.filter(type='ART')
-    conf_proc = Paper.objects.filter(type='CPR')	
-    return render(request, 'publications.html', {'people': people, 'articles': articles, 'conf_proc': conf_proc})
+    conf_procs = Paper.objects.filter(type='CPR')
+    monos = Paper.objects.filter(type='MNG')
+    preprints = Paper.objects.filter(type='PPR')
+    other = Paper.objects.filter(type='OTH')	
+    context = {'people': people, 
+               'years': years,
+               'articles': articles,
+               'conf_procs': conf_procs, 
+               'monos': monos, 
+               'preprints': preprints,
+               'other': other}
+    return render(request, 'publications.html', context)
 
 
 
