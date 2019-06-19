63d9bfd9f4b0b7cf78a4f72ffc225d124eee77a0
Vadim V. Shepelev <vadim.v.shepelev@gmail.com>
2019-06-19 16:42:27 +0300

    cosmetic

--- dcmtsite/www/views.py
+++ dcmtsite/www/views.py
@@ -65,7 +65,7 @@ def researcher(request, researcher_slug):
     researcher = Person.objects.get(slug=researcher_slug)
     # papers = Paper.objects.filter(authors__contains==scientist.family_name)
     papers = Paper.objects.all()
-    context = {'researcher_slug': researcher_slug, 'researcher': researcher, }
+    context = {'researcher_slug': researcher_slug, 'researcher': researcher, 'papers': papers }
     return render(request, 'researcher_card.html', context)
 	
 	
