ba400e581dcea5a0f75ea87b4c255463aaa17742
Vadim V. Shepelev <vadim.v.shepelev@gmail.com>
2019-06-19 15:30:35 +0300

    cosmetic

--- dcmtsite/www/urls.py
+++ dcmtsite/www/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
 	url(r'^scientists/fortova$', views.fortova),
 	url(r'^scientists/shepelev1$', views.shepelev),
 #	url(r'^scientists/shepelev$', views.researcher(slug='shepelev'),
-#	url(r'^scientists/?P<slug>$', views.researcher), 
+	url(r'^scientists/?P<slug>a-z$', views.researcher(researcher_slug=slug), 
 	url(r'^robots.txt$', views.robots_txt),
 ]
