3956289b4f8f273664b5be95591dd7ced8c831c8
Vadim V. Shepelev <vadim.v.shepelev@gmail.com>
2019-06-25 16:33:55 +0300

    cosmetic

--- dcmtsite/www/views.py
+++ dcmtsite/www/views.py
@@ -63,8 +63,9 @@ def robots_txt(request):
 	
 def researcher(request, researcher_slug):
     researcher = Person.objects.get(slug=researcher_slug)
-    # papers = Paper.objects.filter(authors__contains==scientist.family_name)
-    papers = Paper.objects.filter(researcher in Paper.dcmt_authors)
+    # papers = Paper.objects.filter(authors__contains==scientist.family_name)    
+    #papers = Paper.objects.filter(researcher in Paper.dcmt_authors)
+    papers = Paper.objects.all().exclude(researcher in dcmt_authors==false)
     context = {'researcher_slug': researcher_slug, 'researcher': researcher, 'papers': papers }
     return render(request, 'researcher_card.html', context)
 	
